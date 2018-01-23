3d30da2a3c49874a7a77a501d8402bbf68f79077
DuccioLenkowicz <ducciolenkowicz@gmail.com>
2018-01-23 11:42:41 +0100

    added a first c++ gfx implementation

---
+++ tests/gfx.cpp
@@ -0,0 +1,61 @@
+#include<SDL.h>
+
+struct gfx
+{
+    void *window;
+    void *renderer;
+
+    gfx();
+    ~gfx();
+};
+
+struct gfx_safe
+{
+    gfx *gfx_ptr;
+    gfx_safe()
+    {
+        gfx_ptr = new gfx();
+
+        if (SDL_Init(SDL_INIT_VIDEO))
+            delete gfx_ptr;
+
+        SDL_Window *window = SDL_CreateWindow("ciaone",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            600,
+            600,
+            0);
+
+        if (!window)
+            delete gfx_ptr;
+
+        gfx_ptr->window = window;
+
+        SDL_Renderer *renderer = SDL_CreateRenderer(gfx_ptr->window, -1, 0);
+
+        if (!renderer)
+            delete gfx_ptr;;
+
+        gfx_ptr->renderer = renderer;
+    }
+
+    ~gfx_safe()
+    {
+        delete gfx_ptr;
+    }
+};
+
+gfx::gfx()
+{
+    window = NULL;
+    renderer = NULL;
+}
+
+gfx::~gfx()
+{
+    if(this->window)
+        SDL_DestroyWindow(this->window);
+    if(this->renderer)
+        SDL_DestroyRenderer(this->renderer);
+    SDL_Quit();
+}
